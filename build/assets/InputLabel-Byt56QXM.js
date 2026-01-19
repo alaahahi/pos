c1b538c3bddfac618f763fab9d5273bc61b84717
alaahahi <alaa.hahe@gmail.com>
2026-01-19 11:15:40 +0300

    w

--- build/assets/InputLabel-Byt56QXM.js
+++
@@ -1,1 +0,0 @@
-import{a as e,c as t,t as o,q as n}from"./app-CDSy9wae.js";const l={class:"block font-medium text-sm text-gray-700"},c={key:0},r={key:1},p={__name:"InputLabel",props:{value:{type:String}},setup(s){return(a,_)=>(e(),t("label",l,[s.value?(e(),t("span",c,o(s.value),1)):(e(),t("span",r,[n(a.$slots,"default")]))]))}};export{p as _};
